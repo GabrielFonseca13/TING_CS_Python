40f74923bf373f2c5e4a633036da578ed279ef07
Gabriel Fonseca <gabriel.fonseca@live.com>
2023-07-05 11:11:24 -0300

    implementando exists word

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,22 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = list()
+
+    for i in range(len(instance)):
+        file_data = instance.search(i)
+        lines_with_word = {
+            "palavra": word,
+            "arquivo": file_data["nome_do_arquivo"],
+            "ocorrencias": list(),
+        }
+
+        for line, content in enumerate(file_data["linhas_do_arquivo"]):
+            if word.lower() in content.lower():
+                lines_with_word["ocorrencias"].append({"linha": line + 1})
+
+            if len(lines_with_word["ocorrencias"]) > 0:
+                result.append(lines_with_word)
+
+    return result
 
 
 def search_by_word(word, instance):
