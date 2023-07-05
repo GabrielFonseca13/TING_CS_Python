83722431e7c68b5539ac37ff42f65bb2fc67f6e8
Gabriel Fonseca <gabriel.fonseca@live.com>
2023-07-05 11:24:55 -0300

    fix identation

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -13,11 +13,32 @@ def exists_word(word, instance):
             if word.lower() in content.lower():
                 lines_with_word["ocorrencias"].append({"linha": line + 1})
 
-            if len(lines_with_word["ocorrencias"]) > 0:
-                result.append(lines_with_word)
+        if len(lines_with_word["ocorrencias"]) > 0:
+            result.append(lines_with_word)
 
     return result
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = list()
+
+    for i in range(0, len(instance)):
+        file_data = instance.search(i)
+
+        file_data = instance.search(i)
+        lines_with_word = {
+            "palavra": word,
+            "arquivo": file_data["nome_do_arquivo"],
+            "ocorrencias": list(),
+        }
+
+        for line, content in enumerate(file_data["linhas_do_arquivo"]):
+            if word.lower() in content.lower():
+                lines_with_word["ocorrencias"].append(
+                    {"linha": line + 1, "conteudo": content}
+                )
+
+        if len(lines_with_word["ocorrencias"]) > 0:
+            result.append(lines_with_word)
+
+    return result
