69fff21ecb464b4bddd95416f4630d672067e150
Gabriel Fonseca <gabriel.fonseca@live.com>
2023-07-04 23:47:53 -0300

    implementando função remove

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -20,7 +20,11 @@ def process(path_file, instance):
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    if len(instance) == 0:
+        return print("Não há elementos", file=sys.stdout)
+    file = instance.dequeue()
+    removed_file = file["nome_do_arquivo"]
+    print(f"Arquivo {removed_file} removido com sucesso", file=sys.stdout)
 
 
 def file_metadata(instance, position):
