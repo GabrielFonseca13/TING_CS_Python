2444cfe9e26c2b0f70d2f2485b84fe5ad243710d
Gabriel Fonseca <gabriel.fonseca@live.com>
2023-07-04 23:40:13 -0300

    implementando process

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,22 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    content = txt_importer(path_file)
+    dict_content = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(content),
+        "linhas_do_arquivo": content,
+    }
+
+    for i in range(len(instance)):
+        data = instance.search(i)
+        if data == dict_content:
+            return
+
+    instance.enqueue(dict_content)
+    print(dict_content, file=sys.stdout)
 
 
 def remove(instance):
